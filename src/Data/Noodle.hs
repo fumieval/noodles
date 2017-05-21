ee695c5a463fdf87e2a812bda83303a38311e1b2
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-05-21 17:45:13 +0900

    rebuild

--- src/Data/Noodle.hs
+++ src/Data/Noodle.hs
@@ -1,53 +1,88 @@
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneDeriving, DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses #-}
-module Data.Noodle (Noodle, insertL, insertR) where
+module Data.Noodle (Noodle
+    , insert
+    , fromAscList
+    , Data.Noodle.toList
+    , minViewWithKey
+    , maxViewWithKey
+    , splitL
+    , splitR
+    , lookupLE
+    , lookupGT) where
 
+import Data.Foldable as F
 import Data.FingerTree as FT
-import Data.AdditiveGroup
-import Data.AffineSpace
 import Data.List (foldl')
 
-instance AdditiveGroup p => Measured (Sum p) (Item p a) where
-  measure (Spacer p) = Sum p
-  measure _ = mempty
+newtype MaxMaybe a = MaxMaybe { getMaxMaybe :: Maybe a }
 
-data Item p a = Item !a | Spacer !p deriving (Eq, Ord)
+instance Ord i => Monoid (MaxMaybe i) where
+  mempty = MaxMaybe Nothing
+  mappend (MaxMaybe (Just a)) (MaxMaybe (Just b)) = MaxMaybe $ Just (max a b)
+  mappend (MaxMaybe Nothing) (MaxMaybe b) = MaxMaybe b
+  mappend (MaxMaybe a) (MaxMaybe Nothing) = MaxMaybe a
 
-data Noodle p a = Noodle !p !(FingerTree (Sum (Diff p)) (Item (Diff p) a)) !p
-  -- Invariant: ∀(Noodle p s q). q .-. p == measure s
-  | Empty
+instance Ord k => Measured (MaxMaybe k) (Item k a) where
+  measure (Item p _) = MaxMaybe $ Just p
 
-deriving instance (Eq p, Eq (Diff p), Eq a) => Eq (Noodle p a)
-deriving instance (Ord p, Ord (Diff p), Ord a) => Ord (Noodle p a)
+data Item p a = Item !p !a deriving (Eq, Ord, Functor)
 
-instance (AffineSpace p, Show p, Show a) => Show (Noodle p a) where
+newtype Noodle k a = Noodle { unNoodle :: FingerTree (MaxMaybe k) (Item k a) }
+
+instance Functor (Noodle k) where
+  fmap f = Noodle . unsafeFmap (fmap f) . unNoodle
+
+instance (Show k, Show a) => Show (Noodle k a) where
   showsPrec p xs = showParen (p > 10)
-    $ showString "fromList " . shows (toList xs)
+    $ showString "fromList " . shows (Data.Noodle.toList xs)
 
-instance AffineSpace p => Monoid (Noodle p a) where
-  mempty = Empty
-  mappend (Noodle p0 s p1) (Noodle q0 t q1)
-    = Noodle p0 (s >< Spacer (q0 .-. p1) <| t) q1
+instance Ord k => Foldable (Noodle k) where
+  foldMap f (Noodle ft) = foldMap (\(Item _ a) -> f a) ft
+  null (Noodle ft) = FT.null ft
 
-insertL :: AffineSpace p => p -> a -> Noodle p a -> Noodle p a
-insertL p a Empty = Noodle p (FT.singleton (Item a)) p
-insertL p a (Noodle p0 s p1) = Noodle p (Item a <| Spacer (p0 .-. p) <| s) p1
+instance Ord k => Monoid (Noodle k a) where
+  mempty = Noodle mempty
+  mappend (Noodle l) (Noodle r)
+    | _ :> Item x0 _ <- FT.viewr l
+    , Item x1 _ :< _ <- FT.viewl r
+    , x0 <= x1 = Noodle (mappend l r)
+    | FT.null l = Noodle r
+    | FT.null r = Noodle l
+    | otherwise = error "TODO"
 
-insertR :: AffineSpace p => Noodle p a -> p -> a -> Noodle p a
-insertR Empty p a = Noodle p (FT.singleton (Item a)) p
-insertR (Noodle p0 s p1) p a = Noodle p0 (s |> Spacer (p .-. p1) |> Item a) p
+insert :: Ord k => k -> a -> Noodle k a -> Noodle k a
+insert k a (Noodle ft)
+  | _ :> Item k' _ <- FT.viewr ft, k >= k' = Noodle (ft |> Item k a)
+  | FT.null ft = Noodle $ FT.singleton (Item k a)
+  | otherwise = error "TODO"
 
-fromList :: AffineSpace p => [(p, a)] -> Noodle p a
-fromList = foldl' (uncurry . insertR) Empty
+fromAscList :: Ord k => [(k, a)] -> Noodle k a
+fromAscList xs = Noodle $ FT.fromList [ Item k v | (k, v) <- xs ]
 
-toList :: AffineSpace p => Noodle p a -> [(p, a)]
-toList (Noodle p0 s _) = foldr
-  (\e k p -> case e of
-    Item a -> (p, a) : k p
-    Spacer d -> k (p .+^ d)) (const []) s p0
+toList :: Noodle k a -> [(k, a)]
+toList (Noodle ft) = [(k, v) | Item k v <- F.toList ft]
 
-break :: AffineSpace p => (p -> Bool) -> Noodle p a -> (Noodle p a, Noodle p a)
-break cond (Noodle p0 s p1) = (Noodle p0 l p, Noodle p r p1) where
-  (l, r) = split (cond . (p0 .+^) . getSum) s
-  p = p0 .+^ getSum (measure l)
-break _ Empty = (Empty, Empty)
+minViewWithKey :: Ord k => Noodle k a -> Maybe ((k, a), Noodle k a)
+minViewWithKey (Noodle ft) = case FT.viewl ft of
+  EmptyL -> Nothing
+  Item k a :< ft' -> Just ((k, a), Noodle ft')
+
+maxViewWithKey :: Ord k => Noodle k a -> Maybe ((k, a), Noodle k a)
+maxViewWithKey (Noodle ft) = case FT.viewr ft of
+  EmptyR -> Nothing
+  ft' :> Item k a -> Just ((k, a), Noodle ft')
+
+splitL :: Ord k => k -> Noodle k a -> (Noodle k a, Noodle k a)
+splitL k (Noodle ft) = (Noodle l, Noodle r)
+  where (l, r) = FT.split (maybe False (>k) . getMaxMaybe) ft
+
+splitR :: Ord k => k -> Noodle k a -> (Noodle k a, Noodle k a)
+splitR k (Noodle ft) = (Noodle l, Noodle r)
+  where (l, r) = FT.split (maybe False (>=k) . getMaxMaybe) ft
+
+lookupLE :: Ord k => k -> Noodle k a -> Maybe (k, a)
+lookupLE k = fmap fst . maxViewWithKey . fst . splitL k
+
+lookupGT :: Ord k => k -> Noodle k a -> Maybe (k, a)
+lookupGT k = fmap fst . minViewWithKey . snd . splitL k
